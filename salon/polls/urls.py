ce1ec199c493bfeda5920813288bb832656ee4bc
Katie <pie10101011@gmail.com>
2024-02-18 18:24:23 -0800

    added voting

--- salon/polls/urls.py
+++ salon/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/  /polls/1/  /polls/1/results/  /polls/1/vote/
     path("", views.index, name="index"),
